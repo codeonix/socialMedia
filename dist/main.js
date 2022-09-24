48b994dde7c3136e60ebbe822ad699f076e55692
Suraj Kumar Singh <suraj.singh@primathon.in>
2022-09-25 04:06:07 +0530

    add view enjine ejs and tests

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+const core_1 = require("@nestjs/core");
+const path_1 = require("path");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const logger = new common_1.Logger();
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
+    app.setBaseViewsDir((0, path_1.join)(__dirname, '..', 'views'));
+    app.setViewEngine("ejs");
+    await app.listen(3000);
+    logger.log('App is running on port 3000');
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { HomeModule } from './modules/home/home.module';
 
 @Module({
-  imports: [],
+  imports: [HomeModule],
   controllers: [],
   providers: [],
 })
